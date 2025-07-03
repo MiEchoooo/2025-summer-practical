39434b5cc5094c391d9389cdfba86b0e42caeaba
MiEchoooo <3525910649@qq.com>
2025-07-03 21:31:35 +0800

    react

--- front/my-react-app/src/App.js
+++ front/my-react-app/src/App.js
@@ -1,26 +1,134 @@
-import logo from './logo.svg';
-import './App.css';
-import MyComponent from './5-1';
+// import { useState } from "react";
 
-function App() {
+// function MyButton() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount(count + 1);
+//   }
+//   return (
+//     <button onClick={handleClick}>
+//       点了{count}次
+//     </button>
+//   )
+// }
+
+// function App() {
+//   return(
+//     <div>
+//     <h2>独立按钮</h2>
+//     <MyButton />
+//     <MyButton />
+//     </div>
+//   )
+// }
+
+
+
+// function App() {
+//   const [count, setCount] = useState(0);
+
+//   function handleClick() {
+//     setCount (count + 1);
+//   }
+
+//   return (
+//     <div>
+//       <h2>关联按钮</h2>
+//       <MyButton count={count} onClick={handleClick} />
+//       <MyButton count={count} onClick={handleClick} />
+//     </div>
+//   )
+// }
+
+// function MyButton({count, onClick}) {
+//   return (
+//     <button onClick={onClick}>
+//       点了{count}次
+//     </button>
+//   )
+// }
+
+// export default App;
+
+
+
+//第一个练习： 井字棋小游戏项目
+
+import { useState } from "react";
+
+function Square({value, onSquareClick}) {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button  className="square" onClick={onSquareClick}>{value}</button>
+  )
+}
+
+export default function Board() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [squares, setSquares] = useState(Array(9).fill(null));
+
+  function handleClick(i) {
+    if (squares[i] || calculateWinner(squares)) {
+      return;
+    }
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[i] = 'X';
+    }
+    else {
+      nextSquares[i] = 'O';
+    }
+    setSquares(nextSquares);
+    setXIsNext(!xIsNext);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = 'Winner' + winner;
+  }
+  else {
+    status = 'Next player: ' + (xIsNext ? 'X' : 'O');
+  }
+
+  return (
+    <>
+      <div>{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </>
   );
 }
 
-export default App;
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i ++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[b] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
